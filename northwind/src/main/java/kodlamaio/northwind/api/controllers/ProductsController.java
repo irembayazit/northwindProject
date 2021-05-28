45bd9c811631d3ed65bef754370b47db0a1635e1
irem <bayzt.irem@gmail.com>
2021-05-29 00:49:26 +0300

    first commit

---
+++ northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -0,0 +1,40 @@
+package kodlamaio.northwind.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.entities.concretes.Product;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductsController {
+	
+	private ProductService productService;
+	
+	@Autowired //tum projeyi tarar ve productManager'i ve ordan productService'i newler sprin tarafında
+	//@Autowired --> ProductService 'i implement eden classları bulur ve newler
+	public ProductsController(ProductService productService) {
+		super();
+		this.productService = productService;
+	}
+
+	@GetMapping("/getall")
+	public DataResult<List<Product>> getAll(){
+		return this.productService.getAll();
+	}
+	
+	@PostMapping("/add")
+	//@RequestBody ile eklenicek olan bilgileri json'a cevirir
+	public Result add(@RequestBody Product product) {
+		return this.productService.add(product);				
+	}	
+}
---
+++ northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -0,0 +1,40 @@
+package kodlamaio.northwind.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
+import kodlamaio.northwind.core.utilities.results.SuccessResult;
+import kodlamaio.northwind.dataAccesss.abstracts.ProductDao;
+import kodlamaio.northwind.entities.concretes.Product;
+
+@Service //business kısmının service'ini gorucek anlamına geliyor
+public class ProductManager implements ProductService{
+
+	private ProductDao productDao;
+	
+	@Autowired //interface'lerden gelen sonucları alır,ProductDao,ProductService ikisi de interface
+	public ProductManager(ProductDao productDao) {
+		super();
+		this.productDao = productDao;
+	}
+
+	@Override
+	public DataResult<List<Product>> getAll() {
+		return new SuccessDataResult<List<Product>>
+		(this.productDao.findAll(),"Data listelendi");
+				
+	}
+
+	@Override
+	public Result add(Product product) {
+		this.productDao.save(product);
+		return new SuccessResult("ürün eklendi");
+	}
+
+} 
